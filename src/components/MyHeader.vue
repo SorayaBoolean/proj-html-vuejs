8dd03c3a6ae05f50edd7443b79e9c168cb1f4649
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-09-06 13:45:07 +0200

    three components registered imported and used , now I will create general scss rules

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    ciao
+  </div>
+</template>
+
+<script>
+export default {
+name: 'MyHeader'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    come
+  </div>
+</template>
+
+<script>
+export default {
+name: 'MyMain'
+}
+</script>
+
+<style>
+
+</style>
